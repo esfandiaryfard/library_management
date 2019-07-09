ea6afe1b02758108bc2989f954abe7a39b8d42e6
Anita Esf <anita.kntu@gmail.com>
2019-07-10 01:15:48 +0430

    Add model book

--- library/models/books.py
+++ library/models/books.py
@@ -1,5 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext as _
+
+"""
+Create model Book
+"""
 
 
 class Book(models.Model):
-    pass
+    name = models.CharField(max_length=256, verbose_name=_('name'), null=True)
+    ISBN = models.CharField(max_length=256, verbose_name=_('last name'), null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.CharField(max_length=50)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    updated_by = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = _('book')
+        app_label = "library"
+        db_table = "books"
+
+    def __str__(self):
+        return self.name
